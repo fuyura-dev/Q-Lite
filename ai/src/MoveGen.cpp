f968a5ad48a9c092d8213fd40cd015eea88c18af
jlallas384 <jlallas384@gmail.com>
2026-05-12 21:40:38 +0800

    use coroutines for movegen

--- ai/src/MoveGen.cpp
+++ ai/src/MoveGen.cpp
@@ -1,6 +1,21 @@
 #include "MoveGen.h"
 #include <algorithm>
-#include <ranges>
+
+enum class CellSide : uint8_t {
+	kRightSide,
+	kBottomSide,
+	kLeftSide,
+	kTopSide
+};
+
+using CellSideVector = std::pair<CellSide, GridPosition>;
+
+inline constexpr CellSideVector kAdjacent[4] = {
+	{CellSide::kRightSide, {.row = 0, .col = 1}},
+	{CellSide::kBottomSide, {.row = 1, .col = 0}},
+	{CellSide::kLeftSide, {.row = 0, .col = -1}},
+	{CellSide::kTopSide, {.row = -1, .col = 0}}
+};
 
 namespace {
 
@@ -26,106 +41,73 @@ bool HasWall(const Position& pos, GridPosition wall_pos, const CellSideVector& c
 
 }
 
-void AdjacentMoveList::Iterator::Advance() {
-	while (current_vector != std::end(kAdjacent)) {
-		auto cell_side_vector = *current_vector++;
-		GridPosition new_grid_pos = move_list->grid_pos + cell_side_vector.second;
-		if (InBounds(new_grid_pos) && !HasWall(move_list->pos, move_list->grid_pos, cell_side_vector)) {
-			ret = new_grid_pos;
-			return;
+coro::generator<GridPosition> AdjacentMoveList(GridPosition grid_pos, const Position& pos) {
+	for (auto current_vector : kAdjacent) {
+		GridPosition new_grid_pos = grid_pos + current_vector.second;
+		if (InBounds(new_grid_pos) && !HasWall(pos, grid_pos, current_vector)) {
+			co_yield new_grid_pos;
 		}
-	}
-	done = true;
-}
-
-PawnMoveList::PawnMoveList(const Position& pos) : MoveListCommon(pos),
-	player(pos.GetPawnPosition(pos.GetCurrentTurn())),
-	other(pos.GetPawnPosition(pos.GetCurrentTurn() == kWhite ? kBlack : kWhite)),
-	adjacent(player, pos),
-	jump_moves(other, pos) {
-}
-
-void PawnMoveList::Iterator::Advance() {
-	if (!jumping) {
-		NextAdjacent();
-	} else {
-		NextJumping();
 	}
 }
 
-void PawnMoveList::Iterator::NextAdjacent() {
-	while (current != move_list->adjacent.end()) {
-		GridPosition grid_pos = *current;
-		++current;
-		if (grid_pos != move_list->other) {
-			ret = grid_pos;
-			return;
+coro::generator<GridPosition> PawnMoveList(const Position& pos) {
+	GridPosition player = pos.GetPawnPosition(pos.GetCurrentTurn());
+	GridPosition other = pos.GetPawnPosition(pos.GetCurrentTurn() == kWhite ? kBlack : kWhite);
+	bool will_jump = false, straight_jump = false;
+
+
+	for (auto current : AdjacentMoveList(player, pos)) {
+		if (current != other) {
+			co_yield current;
+		} else {
+			will_jump = true;
+			GridPosition vector = other - player;
+			GridPosition jump = other + vector;
+			if (std::ranges::contains(AdjacentMoveList(other, pos), jump)) {
+				straight_jump = true;
+			}
 		}
-		will_jump = true;
-		GridPosition vector = move_list->other - move_list->player;
-		GridPosition jump = move_list->other + vector;
-		if (std::ranges::find(move_list->jump_moves, jump) != move_list->jump_moves.end()) {
-			straight_jump = true;
-		}
+
 	}
-	if (!will_jump) {
-		done = true;
-	} else if (will_jump && current == move_list->adjacent.end()) {
-		jumping = true;
-		current = move_list->jump_moves.begin();
-		NextJumping();
+
+	if (will_jump) {
+		for (auto current : AdjacentMoveList(other, pos)) {
+			GridPosition grid_pos = current;
+			if (grid_pos == player) {
+				continue;
+			}
+			bool same_row_or_col = grid_pos.row == player.row ||
+				grid_pos.col == player.col;
+			if (same_row_or_col == straight_jump) {
+				co_yield grid_pos;
+			}
+		}
 	}
 }
 
-void PawnMoveList::Iterator::NextJumping() {
-	while (current != move_list->jump_moves.end()) {
-		GridPosition grid_pos = *current;
-		++current;
-		if (grid_pos == move_list->player) {
-			continue;
-		}
-		bool same_row_or_col = grid_pos.row == move_list->player.row || 
-								grid_pos.col == move_list->player.col;
-		if (same_row_or_col == straight_jump) {
-			ret = grid_pos;
-			return;
-		}
-	}
-	done = true;
-}
-
-void AllMoveList::Iterator::Advance() {
-	while (pawn_current != move_list->pawn_moves.end()) {
-		GridPosition grid_pos = *pawn_current;
-		++pawn_current;
-		ret = {
+coro::generator<Move> AllMoveList(const Position& pos) {
+	for (auto grid_pos : PawnMoveList(pos)) {
+		co_yield Move{
 			.kind = MoveKind::kMovePawn,
 			.pos = grid_pos,
 			.side = std::nullopt
 		};
-		return;
-	}
-	if (move_list->pos.GetRemainingWalls(move_list->pos.GetCurrentTurn()) == 0) {
-		done = true;
-		return;
 	}
 
-	while (wall_current != kTotalCells) {
+	if (pos.GetRemainingWalls(pos.GetCurrentTurn()) == 0) {
+		co_return;
+	}
+
+	for (uint8_t wall_current = 0; wall_current < kTotalCells; wall_current++) {
 		GridPosition grid_pos = GridPosition::from_compressed(wall_current);
-		WallSide side = current_side;
-		current_side = static_cast<WallSide>(!current_side);
-		if (current_side == kRightSide) {
-			wall_current++;
-		}
-
-		if (move_list->pos.CanPlaceWall(grid_pos, side)) {
-			ret = {
-				.kind = MoveKind::kPlaceWall,
-				.pos = grid_pos,
-				.side = side
-			};
-			return;
+		for (WallSide side : {kRightSide, kBottomSide}) {
+			if (pos.CanPlaceWall(grid_pos, side)) {
+				co_yield Move{
+					.kind = MoveKind::kPlaceWall,
+					.pos = grid_pos,
+					.side = side
+				};
+			}
 		}
 	}
-	done = true;
 }
